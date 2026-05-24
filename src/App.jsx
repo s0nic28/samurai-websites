0f576c7f3f7d40ccb02a8fec9aaea2b5beaf417c
s0nic28 <narutomithun28@gmail.com>
2026-05-24 20:15:53 +0530

    fix render server folder

--- src/App.jsx
+++ src/App.jsx
@@ -129,7 +129,7 @@ function App() {
   e.preventDefault();
 
   try {
-    const res = await fetch("https://samurai-websites.onrender.com", {
+    const res = await fetch("https://samurai-websites.onrender.com/register", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
